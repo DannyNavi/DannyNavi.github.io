b85494d10e1cdcc3567ee802981e60c96680c425
DannyNavi <nguyeda3@oregonstate.edu>
2025-07-15 17:21:49 -0700

    Added back register client

--- server/routes/clientRoutes.js
+++ server/routes/clientRoutes.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const { registerClient} = require('../controllers/clientController');
 
 const router = express.Router();
 const { getAllClients } = require('../controllers/clientController');
+
+
+router.post('/register', registerClient);
 
 router.get('/', getAllClients); // GET /api/clients
 
