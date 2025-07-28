cab2ae1d2dabc9c5a55225e4de32f8b1b20815cf
DannyNavi <nguyeda3@oregonstate.edu>
2025-07-28 16:45:36 -0700

    Fixed endpoint

--- pretty-hair/src/components/client/ClientFromPhone.js
+++ pretty-hair/src/components/client/ClientFromPhone.js
@@ -10,7 +10,7 @@ function ClientFromPhone(){
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch(`/clients/getClientFromPhone/${phone}`);
+      const res = await fetch(`https://dannynavi-github-io.onrender.com/api/clients/getClientFromPhone/${phone}`);
       if (!res.ok) throw new Error('Client not found');
       const data = await res.json();
       navigate(`/viewclient/${data.id}`);
