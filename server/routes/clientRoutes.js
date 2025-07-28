dc5d19378f9e30a435d8445fea16904d420b7fad
DannyNavi <nguyeda3@oregonstate.edu>
2025-07-28 16:54:47 -0700

    Working on client query by phone

--- server/routes/clientRoutes.js
+++ server/routes/clientRoutes.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const { registerClient, deleteClient, getAllClients, updateClient, getClientById, getClientFromPhone} = require('../controllers/clientController');
+const {
+  registerClient,
+  deleteClient,
+  getAllClients,
+  updateClient,
+  getClientById,
+  getClientFromPhone
+} = require('../controllers/clientController');
+
 const router = express.Router();
 
 router.post('/register', registerClient);
 
+router.get('/getClientFromPhone/:cell', getClientFromPhone)
+
+
 router.get('/', getAllClients); // GET /api/clients
 router.put('/:id', updateClient);
 router.delete('/:id', deleteClient);
-router.get('getClientFromPhone/:cell', getClientFromPhone)
 
 module.exports = router;
